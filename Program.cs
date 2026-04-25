61449dd6eab64c4ae9c88256067dcd0706eecba9
Ygor <ygorsouzasimao@gmail.com>
2026-04-24 21:38:45 -0300

    IMC

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double altura, peso, imc;
+Console.Write("Altura (m)..: ");
+altura = Convert.ToDouble(Console.ReadLine());
+Console.Write("Peso (kg)...: ");
+peso = Convert.ToDouble(Console.ReadLine());
+imc = peso / Math.Pow(altura, 2);
+Console.WriteLine($"\nIMC: {imc:N1} kg/m²");
